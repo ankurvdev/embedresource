cf6d790cd71a97a78257449a4ad7a5711e0843ca
Ankur Verma <ankurv@microsoft.com>
2022-09-22 14:42:50 -0700

    Fix portname

--- ci/build_with_vcpkg.py
+++ ci/build_with_vcpkg.py
@@ -59,7 +59,7 @@ parser.add_argument("--host-triplet", type=str, default=None, help="Triplet")
 parser.add_argument("--runtime-triplet", type=str, default=None, help="Triplet")
 args = parser.parse_args()
 scriptdir = pathlib.Path(__file__).parent.absolute()
-portname = scriptdir.parent.name
+portname = "embedresource"
 workdir = pathlib.Path(args.workdir).absolute()
 workdir.mkdir(exist_ok=True)
 vcpkgroot = (workdir / "vcpkg")
