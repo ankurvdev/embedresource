04a52d63ab9db330f0c4a33f79d846df8d079b61
Ankur Verma <ankurv@microsoft.com>
2022-09-22 14:56:42 -0700

    Fixes

--- ci/build_with_vcpkg.py
+++ ci/build_with_vcpkg.py
@@ -83,6 +83,7 @@ for portdir in (scriptdir / 'vcpkg-additional-ports').glob("*"):
 vcpkgportfile.write_text(vcpkgportfile.read_text().replace('SOURCE_PATH ${SOURCE_PATH}', f'SOURCE_PATH "{scriptdir.parent.as_posix()}"'))
 
 myenv = os.environ.copy()
+myenv['VCPKG_ROOT'] = vcpkgroot.as_posix()
 myenv['VCPKG_KEEP_ENV_VARS'] = 'ANDROID_NDK_HOME'
 myenv['VERBOSE'] = "1"
 if "android" in host_triplet or "android" in runtime_triplet:
