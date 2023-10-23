ac5f430de752d5b607c4fcd8e203b289a9ba9ba8
Ankur Verma <ankurv@microsoft.com>
2023-10-23 13:36:51 -0700

    Sync with vcpkg

--- ci/vcpkg-additional-ports/embedresource/portfile.cmake
+++ ci/vcpkg-additional-ports/embedresource/portfile.cmake
@@ -1,17 +1,9 @@
-# sha512sum can be obtained using
-# wget -O - -q  https://github.com/ankurdev/embedresource/archive/b6429f8b92947273a5e66d5f10210b960616a89d.tar.gz | sha512sum 
-set(commitId acbb1736f46da70bb06d33c53da5bd2dcdc2fc85)
-set(sha512 06c1692f89ed37db8978afb6d878b1882d0e357bc31f5fbdb6a8b6f07041b152d12379ece617a851483d1582d0c2af5443877117f3da9cc0ca6919dfdcfd81fe)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ankurvdev/embedresource
-    REF ${commitId}
-    SHA512 ${sha512}
-    HEAD_REF master)
-
-if(NOT TARGET_TRIPLET STREQUAL HOST_TRIPLET)
-    vcpkg_add_to_path(PREPEND "${CURRENT_HOST_INSTALLED_DIR}/tools")
-endif()
+    REF "v${VERSION}"
+    SHA512 96d2208fd5d654dad5662968296fa363cea0a935fec8474b780717c9303d2dd763833370bcdf02d6d63e264368b0955fa1f13c6e55685280df5fdaf9e72b8c9f
+    HEAD_REF main)
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
@@ -21,16 +13,18 @@ vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-file(RENAME "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/tools")
+if(NOT VCPKG_CROSSCOMPILING)
+    vcpkg_copy_tools(TOOL_NAMES embedresource AUTO_CLEAN)
+endif()
 
 file(READ "${CURRENT_PACKAGES_DIR}/share/embedresource/EmbedResourceConfig.cmake" config_contents)
 file(WRITE "${CURRENT_PACKAGES_DIR}/share/embedresource/EmbedResourceConfig.cmake"
 "find_program(
-    EMBEDRESOURCE_EXECUTABLE embedresource 
-    PATHS 
-        \"\${CMAKE_CURRENT_LIST_DIR}/../../../${HOST_TRIPLET}/tools\"
+    EMBEDRESOURCE_EXECUTABLE embedresource
+    PATHS
+        \"\${CMAKE_CURRENT_LIST_DIR}/../../../${HOST_TRIPLET}/tools/${PORT}\"
     NO_DEFAULT_PATH
     REQUIRED)
 ${config_contents}"
