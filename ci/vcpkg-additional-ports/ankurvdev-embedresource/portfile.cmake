b3828a5a60fb082b0a79351c29b479869e685922
Ankur Verma <31362771+ankurvdev@users.noreply.github.com>
2023-10-26 22:45:31 -0700

    Vcpkg fix warnings due to stray exe's (#15)
    Co-authored-by: Ankur <ankurv@verma>

--- ci/vcpkg-additional-ports/ankurvdev-embedresource/portfile.cmake
+++ ci/vcpkg-additional-ports/ankurvdev-embedresource/portfile.cmake
@@ -17,12 +17,14 @@ vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 if(HOST_TRIPLET STREQUAL TARGET_TRIPLET) # Otherwise fails on wasm32-emscripten
     vcpkg_copy_tools(TOOL_NAMES embedresource AUTO_CLEAN)
+else()
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
 endif()
 
 file(READ "${CURRENT_PACKAGES_DIR}/share/embedresource/EmbedResourceConfig.cmake" config_contents)
 file(WRITE "${CURRENT_PACKAGES_DIR}/share/embedresource/EmbedResourceConfig.cmake"
 "find_program(
-    EMBEDRESOURCE_EXECUTABLE embedresource
+    embedresource_EXECUTABLE embedresource
     PATHS
         \"\${CMAKE_CURRENT_LIST_DIR}/../../../${HOST_TRIPLET}/tools/${PORT}\"
     NO_DEFAULT_PATH
