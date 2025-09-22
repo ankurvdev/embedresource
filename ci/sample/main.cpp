5066c9ec0d1e83bfe67bf2cc75aeb92ebb97acb5
ankurv <ankur@verma>
2025-09-21 22:42:24 -0700

    Fix tests for wide names

--- ci/sample/main.cpp
+++ ci/sample/main.cpp
@@ -6,8 +6,13 @@
 
 DECLARE_RESOURCE_COLLECTION(testdata1);
 DECLARE_RESOURCE_COLLECTION(testdata2);
+#if !(defined EMBEDRESOURCE_NAME_ENCODING_UTF16 && EMBEDRESOURCE_NAME_ENCODING_UTF16 == 1)
 DECLARE_RESOURCE_COLLECTION(testdata3);
 DECLARE_RESOURCE(testdata3, main_cpp);
+#else
+DECLARE_RESOURCE_COLLECTION(testdata3_wide);
+DECLARE_RESOURCE(testdata3_wide, main_cpp);
+#endif
 
 #if !(defined EMBEDRESOURCE_NAME_ENCODING_UTF16 && EMBEDRESOURCE_NAME_ENCODING_UTF16 == 1)
 std::string_view MainCppName = "main.cpp";
@@ -53,8 +58,13 @@ void verify_resource(ResourceLoader const& r)
 int main(int argc, char* argv[])
 try
 {
+#if !(defined EMBEDRESOURCE_NAME_ENCODING_UTF16 && EMBEDRESOURCE_NAME_ENCODING_UTF16 == 1)
     std::string_view res = LOAD_RESOURCE(testdata3, main_cpp).data;
     if (res.size() != MAIN_CPP_FILE_SIZE) { throw std::runtime_error("r.data.len() != MAIN_CPP_FILE_SIZE"); }
+#else
+    std::string_view res = LOAD_RESOURCE(testdata3_wide, main_cpp).data;
+    if (res.size() != MAIN_CPP_FILE_SIZE) { throw std::runtime_error("r.data.len() != MAIN_CPP_FILE_SIZE"); }
+#endif
 
     auto resourceCollection1 = LOAD_RESOURCE_COLLECTION(testdata1);
     for (auto const r : resourceCollection1) { verify_resource(r); }
@@ -62,9 +72,13 @@ try
     auto resourceCollection2 = LOAD_RESOURCE_COLLECTION(testdata2);
     for (auto const r : resourceCollection2) { verify_resource(r); }
 
+#if !(defined EMBEDRESOURCE_NAME_ENCODING_UTF16 && EMBEDRESOURCE_NAME_ENCODING_UTF16 == 1)
     auto resourceCollection3 = LOAD_RESOURCE_COLLECTION(testdata3);
     for (auto const r : resourceCollection2) { verify_resource(r); }
-
+#else
+    auto resourceCollection3 = LOAD_RESOURCE_COLLECTION(testdata3_wide);
+    for (auto const r : resourceCollection2) { verify_resource(r); }
+#endif
     return 0;
 } catch (const std::exception& ex)
 {
