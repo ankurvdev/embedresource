80658f83fb4d389bbdf13914edf0dfcefa80c1d3
Ankur <ankurv@verma>
2026-09-14 23:27:22 -0700

    Fixes

--- cmake/EmbedResourceTargets.cmake
+++ cmake/EmbedResourceTargets.cmake
@@ -25,7 +25,7 @@ FetchContent_Declare(
     GIT_TAG        main
 )
 
-function(_target_add_resource target)
+function(z_embd_target_add_resource target)
     cmake_parse_arguments("" "" "NAME_ENCODING;RESOURCE_COLLECTION_NAME" "RESOURCES;GENERATOR_COMMAND;GENERATOR_DEPEND;GENERATOR_SPECFILE" ${ARGN})
     if (NOT DEFINED _RESOURCE_COLLECTION_NAME)
         set(_RESOURCE_COLLECTION_NAME "${target}")
@@ -94,7 +94,7 @@ endfunction()
 function(add_resource_library target libkind)
     FindOrBuildTool(embedresource)
     add_library(${target} ${libkind})
-    _target_add_resource(${target} ${ARGN})
+    z_embd_target_add_resource(${target} ${ARGN})
 endfunction()
 
 macro(target_add_resource target)
